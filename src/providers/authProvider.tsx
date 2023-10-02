7b0f7a8a7b8fc9dc2feda628961295b683276866
davidadeola <davidolorunsesan15@gmail.com>
2023-10-02 16:01:39 +0100

    fetch salt and get user address, finally!!!

--- src/providers/authProvider.tsx
+++ src/providers/authProvider.tsx
@@ -1,4 +1,11 @@
-import { ReactNode, createContext, useCallback, useEffect } from "react";
+import {
+  ReactNode,
+  useState,
+  createContext,
+  useCallback,
+  useEffect,
+} from "react";
+import { jwtToAddress } from "@mysten/zklogin";
 
 const AuthContext = createContext<{} | null>(null);
 
@@ -7,26 +14,48 @@ interface AuthProviderProps {
 }
 
 const AuthProvider = ({ children }: AuthProviderProps) => {
-  const fetchSalt = useCallback(async () => {
-    const hash = window.location.hash;
-    const params = new URLSearchParams(hash.slice(1));
-    const idToken = params.get("id_token");
+  const [userToken, setUserToken] = useState<string | null>(null);
+  const [address, setAddress] = useState<string | null>("");
 
+  // Fetch salt
+  const fetchSalt = useCallback(async (idToken: string) => {
     if (!idToken) return;
-
     try {
       const response = await fetch(`/api/auth?token=${idToken}`);
       const result = await response.json();
-      const salt = result.salt;
-      return salt;
+      if (result && typeof result !== "object") {
+        throw new Error("Response is not in JSON format");
+      }
+      return result.salt;
     } catch (error) {
       console.error(error);
     }
   }, []);
 
+  const getAddress = useCallback(async (token: string, salt: bigint) => {
+    const userAddress = jwtToAddress(token, salt);
+    setAddress(userAddress);
+  }, []);
+
+  const fetchSaltAndGetAddress = useCallback(async () => {
+    if (userToken) {
+      const salt = await fetchSalt(userToken);
+      console.log(salt);
+      if (salt) getAddress(userToken, salt);
+    }
+  }, [fetchSalt, getAddress, userToken]);
+  console.log(address);
+
   useEffect(() => {
-    fetchSalt();
-  }, [fetchSalt]);
+    fetchSaltAndGetAddress();
+  }, [fetchSaltAndGetAddress]);
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    const params = new URLSearchParams(hash.slice(1));
+    const idToken = params.get("id_token");
+    setUserToken(idToken);
+  }, []);
 
   return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
 };
